ced41820efd345e238a4e9e27fe33c6326fe2a53
Atig-Hamza <hamzaatig@icloud.com>
2025-12-22 14:01:42 +0100

    feat: add OrganizationsModule and InvitationsModule to AuthModule imports

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { OrganizationsModule } from '../organizations/organizations.module';
+import { InvitationsModule } from '../invitations/invitations.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -10,6 +12,8 @@ import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [
     UsersModule,
+    OrganizationsModule,
+    InvitationsModule,
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
