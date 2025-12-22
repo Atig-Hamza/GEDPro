e6722fd1b3b9b10e50b8df119e232cc8d961bfa0
Atig-Hamza <hamzaatig@icloud.com>
2025-12-22 14:03:55 +0100

    feat: add organizationId and permissions properties to User schema

--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export type UserDocument = User & Document;
 
@@ -16,6 +16,12 @@ export class User {
 
   @Prop()
   lastName?: string;
+
+  @Prop({ type: Types.ObjectId, ref: 'Organization' })
+  organizationId?: Types.ObjectId;
+
+  @Prop({ type: [String], default: [] })
+  permissions: string[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
